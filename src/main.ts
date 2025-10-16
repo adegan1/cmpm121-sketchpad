701746105e9b0f5a7f38ab1535dc35687419eeee
adegan1 <150628726+adegan1@users.noreply.github.com>
2025-10-16 01:37:36 +0000

    Created new notify and redraw functions. Calls redraw function to update the canvas whenever an update has been made (Step 3 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+"use strict";
+
 // Import assets
 import "./style.css";
 
@@ -17,26 +19,72 @@ const canvas = document.getElementById("sketchpad") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 const cursor = { active: false, x: 0, y: 0 };
 
+const event = new EventTarget();
+
+// Store drawn lines
+type Point = { x: number; y: number };
+const lines: Point[][] = [];
+
+let thisLine: Point[] | null = null;
+
+// Set up canvas context
+ctx.lineWidth = 2;
+ctx.lineCap = "round";
+ctx.strokeStyle = "#000000ff";
+
+// Notify function
+function notify(eventName: string) {
+  event.dispatchEvent(new CustomEvent(eventName));
+}
+
+// Redraw canvas function
+function redraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const line of lines) {
+    if (line.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0];
+      ctx.moveTo(x, y);
+      for (const { x, y } of line) {
+        ctx.lineTo(x, y);
+      }
+      ctx.stroke();
+    }
+  }
+}
+
+event.addEventListener("drawing-changed", redraw);
+event.addEventListener("cursor-changed", redraw);
+
 // Set up event listeners for drawing
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  thisLine = [];
+  lines.push(thisLine);
+  thisLine.push({ x: cursor.x, y: cursor.y });
+
+  notify("cursor-changed");
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    thisLine?.push({ x: cursor.x, y: cursor.y });
   }
+
+  notify("drawing-changed");
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
+  thisLine = null;
+
+  notify("cursor-changed");
 });
 
 // Clear button functionality
