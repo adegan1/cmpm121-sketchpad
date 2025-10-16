d35ca4ca442855691018de533ebb24bfbdf22f85
adegan1 <150628726+adegan1@users.noreply.github.com>
2025-10-16 00:27:03 +0000

    Added a title and canvas with respective css classes (Step 1 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+// Import assets
 import "./style.css";
 
+// Set up the HTML structure
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <h1 class="title">Sketchpad</h1>
+  <canvas class="canvas" id="sketchpad" width="256" height="256"></canvas>
 `;
